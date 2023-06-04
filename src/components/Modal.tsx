d21d9c8149905ffe95307afa7b4ae8ee4d70a443
vendredl <65541546+VENDREDl@users.noreply.github.com>
2023-06-04 18:05:00 +0000

    Update 3 files

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,69 +1,69 @@
 import styled from "styled-components";
 
 const Background = styled.div`
-    position: fixed;
-    top:0; left: 0; bottom: 0; right: 0;
-    background: rgba(0, 0, 0, 0.8);
-`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.8);
+`;
 
 const Container = styled.div`
-    width: 60%;
-    height: 90%;
-    z-index: 3;
-    position: fixed;
-    top: 80px;
-    left: 20%;
-    overflow: hidden;
-`
+  width: 60%;
+  height: 90%;
+  z-index: 3;
+  position: fixed;
+  top: 80px;
+  left: 20%;
+  overflow: hidden;
+  border-radius: 15px;
+`;
 
 const Area = styled.div`
-    width: 100%;
-    height: 100%;
-    position: relative;
-    overflow: hidden
-`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+`;
 
 const Iframe = styled.iframe`
-    position: absolute;
-    top: 0;
-    left: -1px;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-`
+  position: absolute;
+  top: 0;
+  left: -1px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
 
 const Button = styled.button`
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    border-radius: 20px;
-    width: 30px;
-    height: 30px;
-    z-index: 4;
-`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  border-radius: 20px;
+  width: 30px;
+  height: 30px;
+  z-index: 4;
+`;
 
 function Modal({ setModalOpen, id }) {
+  const location = document.location.href;
 
-    console.log(id)
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-    const closeModal = () => {
-        setModalOpen(false);
-    };
-
-    
-
-    return (
-       <Background>
+  return (
+    <>
+      <Background>
         <Container>
-                <Button onClick={closeModal}>
-                    X
-                </Button>
-                <Area>
-                    <Iframe src={`http://221.162.218.180:4000/character/${id}`}></Iframe>
-                </Area>
-                
-            </Container>
-       </Background>
-    );
+          <Button onClick={closeModal}>X</Button>
+          <Area>
+            <Iframe src={`${location}character/${id}`}></Iframe>
+          </Area>
+        </Container>
+      </Background>
+    </>
+  );
 }
 export default Modal;
--- src/pages/CharacterDetailPage.tsx
+++ src/pages/CharacterDetailPage.tsx
@@ -2,79 +2,80 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components"
-import defaultImg from "../imagenotfound.png"
+import styled from "styled-components";
+import defaultImg from "../imagenotfound.png";
 
 const CharacterDiv = styled.div`
-    display: grid;
-    grid-template-rows: 1fr 10fr;
-    justify-items: center;
-    align-items: first baseline;
-    font-family: 'Cherry Bomb One', cursive;
-    font-size: 23px;
-    font-weight: 400;
-    background-color: whitesmoke;
-    height: 80vh;
-    overflow: hidden;
-`
+  display: grid;
+  grid-template-rows: 1fr 10fr;
+  justify-items: center;
+  align-items: first baseline;
+  font-family: "Cherry Bomb One", cursive;
+  font-size: 23px;
+  font-weight: 400;
+  background-color: whitesmoke;
+  height: 80vh;
+  overflow: hidden;
+`;
 
 const CharacterInfo = styled.div`
-    display: grid;
-    grid-template-rows: 2fr 1fr;
-    justify-items: center;
-`
+  display: grid;
+  grid-template-rows: 2fr 1fr;
+  justify-items: center;
+`;
 
 const CharacterImg = styled.img`
-    border-radius: 100px;
-    width: 200px;
-    height: 200px;
-    border: 1px solid black;
-`
+  border-radius: 100px;
+  width: 200px;
+  height: 200px;
+  border: 1px solid black;
+`;
 
 const Films = styled.div`
-    font-size: 30px;
-`
+  font-size: 30px;
+`;
 
 const FilmList = styled.ul`
-    font-size: 20px;
-    margin-top: 20px;
-`
+  font-size: 20px;
+  margin-top: 20px;
+`;
 
 interface CharacterDetailData {
-    id: number;
-    name: string;
-    imageUrl: string;
-    films: string[];
+  id: number;
+  name: string;
+  imageUrl: string;
+  films: string[];
 }
 
-const CharacterDetailPage: React.FC<CharacterDetailData> = ({id, name, imageUrl, films}) => {
+const CharacterDetailPage: React.FC<CharacterDetailData> = ({
+  id,
+  name,
+  imageUrl,
+  films,
+}) => {
+  const urlExists = (url: string) => {
+    const http = new XMLHttpRequest();
+    http.open("HEAD", url, false);
+    http.send();
+    if (http.status !== 404) return true;
+  };
 
-    const urlExists = (url:string) => {
-        const http = new XMLHttpRequest();
-        http.open('HEAD', url, false);
-        http.send();
-        if(http.status !== 404 ) return true 
-    }
+  console.log(films);
 
-    console.log(films)
-
-    return (
-        <CharacterDiv>
-            <div></div>
-            <CharacterInfo>
-                {urlExists(imageUrl) === true ? <CharacterImg src={imageUrl} alt={name}/> : <CharacterImg src={defaultImg} alt={name}/>}
-                <h3>{name}</h3>
-                <Films>Films</Films>
-                <ul>
-                    {
-                        films && films.map(film => (
-                            <FilmList key={film}>{film}</FilmList>
-                        ))
-                    }
-                </ul>
-            </CharacterInfo>
-        </CharacterDiv>
-    )
-}
+  return (
+    <CharacterDiv>
+      <div></div>
+      <CharacterInfo>
+        {/* {urlExists(imageUrl) === true ? <CharacterImg src={imageUrl} alt={name}/> : <CharacterImg src={defaultImg} alt={name}/>} */}
+        <CharacterImg src={imageUrl} alt={name} />
+        <h3>{name}</h3>
+        <Films>Films</Films>
+        <ul>
+          {films && films.map((film) => <FilmList key={film}>{film}</FilmList>)}
+        </ul>
+      </CharacterInfo>
+    </CharacterDiv>
+  );
+};
 
 export default CharacterDetailPage;
